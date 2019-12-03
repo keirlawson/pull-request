0356e7790e7f7fd74a8b64b3194508be5c78478c
Keir Lawson <keirlawson@gmail.com>
2019-12-03 13:34:52 +0000

    Complete fork detection, creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use futures::Stream;
-use tokio::runtime::Runtime;
+use tokio::runtime::current_thread::Runtime;
+use futures::future::Future;
 
 use hubcaps::repositories::{ForkListOptions, Repo};
 use hubcaps::{Credentials, Github, Result};
@@ -10,12 +11,15 @@ pub fn create_pr(organisation: &str, repository: &str) -> Result<()> {
         Credentials::Token("personal-access-token".into()),
       )?;
 
-      let fork = existing_fork("keirlawson", &github, organisation, repository)?;
+      let username = get_username(&github)?;
 
-    //   let fork = fork.or_else(f: F)
-    
-      //check if exists, if not, fork
-      // - check: calculate fork name, check if exists and is fork 
+      let fork = existing_fork(username.as_str(), &github, organisation, repository)?;
+
+      let fork = if let Some(existing) = fork {
+          existing
+      } else {
+        create_fork(&github, organisation, repository)?
+      };
     
       //clone fork
     
@@ -37,7 +41,7 @@ pub fn create_pr(organisation: &str, repository: &str) -> Result<()> {
 }
 
 //FIXME why does user have to be static?
-fn existing_fork(user: &'static str, github: &Github, organisation: &str, repository: &str) -> Result<Option<Repo>> {
+fn existing_fork(user: &str, github: &Github, organisation: &str, repository: &str) -> Result<Option<Repo>> {
     let mut rt = Runtime::new()?;
 
     let options = ForkListOptions::builder().build();
@@ -55,4 +59,27 @@ fn existing_fork(user: &'static str, github: &Github, organisation: &str, reposi
     } else {
         Ok(None)
     }
+}
+
+//FIXME can we share our rt?
+fn create_fork(github: &Github, organisation: &str, repository: &str) -> Result<Repo> {
+    let mut rt = Runtime::new()?;
+    
+    rt.block_on(
+        github
+            .repo(organisation, repository)
+            .forks()
+            .create()
+    )
+}
+
+fn get_username(github: &Github) -> Result<String> {
+    let mut rt = Runtime::new()?;
+    
+    rt.block_on(
+        github
+            .users()
+            .authenticated()
+            .map(move |authed| authed.login)
+    )
 }
