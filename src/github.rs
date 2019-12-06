5039ae07c8cd23b0f06b12785911da574eacc760
Keir Lawson <keirlawson@gmail.com>
2019-12-06 14:52:57 +0000

    Reformat

--- src/github.rs
+++ src/github.rs
@@ -1,86 +1,78 @@
-use tokio::runtime::current_thread::Runtime;
 use futures::future::Future;
 use futures::Stream;
-
+use tokio::runtime::current_thread::Runtime;
 
 use hubcaps::repositories::{ForkListOptions, Repo};
-use hubcaps::{Github, Credentials, Result, pulls::{PullOptions, Pull}};
+use hubcaps::{
+    pulls::{Pull, PullOptions},
+    Credentials, Github, Result,
+};
 
 pub struct GithubClient {
     rt: Runtime,
-    github: Github
+    github: Github,
 }
 
 impl GithubClient {
     pub fn init(user_agent: &str, access_token: &str) -> Result<Self> {
+        let rt = Runtime::new()?;
 
+        let github = Github::new(user_agent, Credentials::Token(access_token.into()))?;
 
-    let rt = Runtime::new()?;
-
-    let github = Github::new(
-        user_agent,
-        Credentials::Token(access_token.into()),
-      )?;
-
-        Ok(GithubClient {
-            rt,
-            github
-        })
+        Ok(GithubClient { rt, github })
     }
 
+    pub fn open_pr(&mut self, organisation: &str, repository: &str, title: &str) -> Result<Pull> {
+        //FIXME fill these in
+        let options = PullOptions {
+            title: String::from(title),
+            head: String::from(""),
+            body: None,
+            base: String::from(""),
+        };
 
-pub fn open_pr(&mut self, organisation: &str, repository: &str, title: &str) -> Result<Pull> {
-
-    //FIXME fill these in
-    let options = PullOptions {
-        title: String::from(title),
-        head: String::from(""),
-        body: None,
-        base: String::from("")
-    };
-
-    self.rt.block_on(
-        self.github
-            .repo(organisation, repository)
-            .pulls()
-            .create(&options)
-    )
-}
-
-pub fn existing_fork(&mut self, user: &str, organisation: &str, repository: &str) -> Result<Option<Repo>> {
-    let options = ForkListOptions::builder().build();
-    let mut forks = self.rt.block_on(
-        self.github
-            .repo(organisation, repository)
-            .forks()
-            .iter(&options)
-            .filter(move |repo| repo.owner.login == user)
-            .collect()
-    )?;
-
-    if !forks.is_empty() {
-        Ok(Some(forks.remove(0)))
-    } else {
-        Ok(None)
+        self.rt.block_on(
+            self.github
+                .repo(organisation, repository)
+                .pulls()
+                .create(&options),
+        )
     }
-}
 
-pub fn create_fork(&mut self, organisation: &str, repository: &str) -> Result<Repo> {
-    self.rt.block_on(
-        self.github
-            .repo(organisation, repository)
-            .forks()
-            .create()
-    )
-}
+    pub fn existing_fork(
+        &mut self,
+        user: &str,
+        organisation: &str,
+        repository: &str,
+    ) -> Result<Option<Repo>> {
+        let options = ForkListOptions::builder().build();
+        let mut forks = self.rt.block_on(
+            self.github
+                .repo(organisation, repository)
+                .forks()
+                .iter(&options)
+                .filter(move |repo| repo.owner.login == user)
+                .collect(),
+        )?;
 
-pub fn get_username(&mut self) -> Result<String> {
-    self.rt.block_on(
-        self.github
-            .users()
-            .authenticated()
-            .map(move |authed| authed.login)
-    )
-}
+        if !forks.is_empty() {
+            Ok(Some(forks.remove(0)))
+        } else {
+            Ok(None)
+        }
+    }
 
+    pub fn create_fork(&mut self, organisation: &str, repository: &str) -> Result<Repo> {
+        self.rt
+            .block_on(self.github.repo(organisation, repository).forks().create())
+    }
+
+    pub fn get_username(&mut self) -> Result<String> {
+        self.rt.block_on(
+            self.github
+                .users()
+                .authenticated()
+                .map(move |authed| authed.login),
+        )
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,63 @@
-use rustygit::{Repository, types::GitUrl};
+use rustygit::{types::GitUrl, Repository};
 use std::str::FromStr;
 use tempdir::TempDir;
 use url::Url;
 
-use hubcaps::{Result};
+use hubcaps::Result;
 
 mod github;
 
 const DEFAULT_UPSTREAM_REMOTE: &str = "upstream";
 
 pub fn create_pr(organisation: &str, repository: &str) -> Result<Url> {
+    let mut github_client =
+        github::GithubClient::init("my-cool-user-agent/0.1.0", "personal-access-token")?;
 
+    let username = github_client.get_username()?;
 
-    let mut github_client = github::GithubClient::init("my-cool-user-agent/0.1.0", "personal-access-token")?;
+    let fork = github_client.existing_fork(username.as_str(), organisation, repository)?;
 
-      let username = github_client.get_username()?;
-
-      let fork = github_client.existing_fork(username.as_str(), organisation, repository)?;
-
-      let fork = if let Some(existing) = fork {
-          existing
-      } else {
+    let fork = if let Some(existing) = fork {
+        existing
+    } else {
         github_client.create_fork(organisation, repository)?
-      };
+    };
 
-      //FIXME allow users to specify path
-      let tmp_dir = TempDir::new("example")?;
+    //FIXME allow users to specify path
+    let tmp_dir = TempDir::new("example")?;
 
+    //FIXME allow user to specify SSH or HTTPS
+    let url = GitUrl::from_str(&fork.ssh_url).expect("github returned malformed clone URL");
 
-      //FIXME allow user to specify SSH or HTTPS
-      let url = GitUrl::from_str(&fork.ssh_url).expect("github returned malformed clone URL");
+    let repo = Repository::clone(url, tmp_dir.path()).unwrap();
 
-      let repo = Repository::clone(url, tmp_dir.path()).unwrap();
-    
-      //FIXME check if upstream remote exists
+    //FIXME check if upstream remote exists
 
-      let upstream = GitUrl::from_str(format!("").as_str()).unwrap();
-      repo.add_remote(DEFAULT_UPSTREAM_REMOTE, &upstream).unwrap();
+    let upstream = GitUrl::from_str(format!("").as_str()).unwrap();
+    repo.add_remote(DEFAULT_UPSTREAM_REMOTE, &upstream).unwrap();
 
-      repo.fetch_remote(DEFAULT_UPSTREAM_REMOTE).unwrap();
-    
-      //FIXME take parameter of branch name
-      repo.create_branch_from_startpoint("somebranchnamehere", format!("{}/{}", DEFAULT_UPSTREAM_REMOTE, fork.default_branch).as_str()).unwrap();
-    
-      //had off to transformation
-    
-      //FIXME need commit message
-      repo.commit_all("somecommitmessage").unwrap();
-    
-      repo.push().unwrap();
-    
-      // open PR
-      let pull = github_client.open_pr(organisation, repository, "sometitlehere").unwrap();
+    repo.fetch_remote(DEFAULT_UPSTREAM_REMOTE).unwrap();
 
-      let url = Url::parse(pull.url.as_str()).unwrap();
+    //FIXME take parameter of branch name
+    repo.create_branch_from_startpoint(
+        "somebranchnamehere",
+        format!("{}/{}", DEFAULT_UPSTREAM_REMOTE, fork.default_branch).as_str(),
+    )
+    .unwrap();
 
-      Ok(url)
+    //had off to transformation
+
+    //FIXME need commit message
+    repo.commit_all("somecommitmessage").unwrap();
+
+    repo.push().unwrap();
+
+    // open PR
+    let pull = github_client
+        .open_pr(organisation, repository, "sometitlehere")
+        .unwrap();
+
+    let url = Url::parse(pull.url.as_str()).unwrap();
+
+    Ok(url)
 }
