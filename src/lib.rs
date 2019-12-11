f44a590b3a6593559a67b6eff47bd1758cf64fbb
Felipe Fill Cardoso <felipefillcardoso@skyscanner.net>
2019-12-11 15:12:09 +0000

    Running rustfmt and editorconfig

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
-use rustygit::{types::{GitUrl, BranchName}, Repository, error::GitError as RustyGitError};
+use github::GithubClient;
+use log::debug;
+use rustygit::{
+    error::GitError as RustyGitError,
+    types::{BranchName, GitUrl},
+    Repository,
+};
+use std::io::Error as ioError;
+use std::path::Path;
+use std::result::Result as stdResult;
 use std::str::FromStr;
 use tempfile;
-use url::{Url, ParseError};
-use log::debug;
-use github::GithubClient;
-use std::path::Path;
 use thiserror::Error;
-use std::result::Result as stdResult;
-use std::io::Error as ioError;
+use url::{ParseError, Url};
 
 use hubcaps::Error as HubcapsError;
 
@@ -33,39 +37,52 @@ pub enum PullRequestError {
     #[error("Unable to create temporary directory")]
     TemporaryDirectoryError(#[from] ioError),
     #[error("Unable to parse pull request URL")]
-    UrlError(#[from] ParseError)
+    UrlError(#[from] ParseError),
 }
 
-type Result<T> = stdResult<T, PullRequestError>; 
+type Result<T> = stdResult<T, PullRequestError>;
 
 // FIXME Use traits to make params more flexible
-pub fn create_enterprise_pr<F>(github_token: &str, user_agent: &str, host: &str, options: &PullRequestOptions, transform: F) -> Result<Url> 
-where F: Fn(&Path) -> Result<()>
+pub fn create_enterprise_pr<F>(
+    github_token: &str,
+    user_agent: &str,
+    host: &str,
+    options: &PullRequestOptions,
+    transform: F,
+) -> Result<Url>
+where
+    F: Fn(&Path) -> Result<()>,
 {
-    let github_client =
-        GithubClient::init(user_agent, github_token, Some(host))?;
+    let github_client = GithubClient::init(user_agent, github_token, Some(host))?;
 
     pr(github_client, options, transform)
 }
 
-pub fn create_pr<F>(github_token: &str, user_agent: &str, options: &PullRequestOptions, transform: F) -> Result<Url> 
-where F: Fn(&Path) -> Result<()>
+pub fn create_pr<F>(
+    github_token: &str,
+    user_agent: &str,
+    options: &PullRequestOptions,
+    transform: F,
+) -> Result<Url>
+where
+    F: Fn(&Path) -> Result<()>,
 {
-    let github_client =
-        GithubClient::init(user_agent, github_token, None)?;
+    let github_client = GithubClient::init(user_agent, github_token, None)?;
 
     pr(github_client, options, transform)
 }
 
 fn pr<F>(mut github_client: GithubClient, options: &PullRequestOptions, transform: F) -> Result<Url>
-    where F: Fn(&Path) -> Result<()>
+where
+    F: Fn(&Path) -> Result<()>,
 {
     //FIXME validate that strings are not empty
 
     let username = github_client.get_username()?;
     debug!("Retrieved username for github account: {}", username);
 
-    let fork = github_client.existing_fork(username.as_str(), options.organisation, options.repository)?;
+    let fork =
+        github_client.existing_fork(username.as_str(), options.organisation, options.repository)?;
 
     let fork = if let Some(existing) = fork {
         existing
@@ -86,7 +103,13 @@ fn pr<F>(mut github_client: GithubClient, options: &PullRequestOptions, transfor
     //FIXME check if upstream remote exists
 
     //FIXME support ssh URLs as well, what about custom githubs?
-    let upstream = GitUrl::from_str(format!("https://github.com/{}/{}.git", options.organisation, options.repository).as_str())?;
+    let upstream = GitUrl::from_str(
+        format!(
+            "https://github.com/{}/{}.git",
+            options.organisation, options.repository
+        )
+        .as_str(),
+    )?;
     repo.add_remote(DEFAULT_UPSTREAM_REMOTE, &upstream)?;
 
     repo.fetch_remote(DEFAULT_UPSTREAM_REMOTE)?;
@@ -100,18 +123,23 @@ fn pr<F>(mut github_client: GithubClient, options: &PullRequestOptions, transfor
     transform(tmp_dir.path())?;
 
     //FIXME update rusty-git to ensure errors are captured
-    repo.add(vec!("."))?;
+    repo.add(vec!["."])?;
     repo.commit_all(options.commit_mesage)?;
-
-
+q
     //FIXME should do this validation at the start
     let upstream_branch = BranchName::from_str(options.branch_name)?;
     repo.push_to_upstream("origin", &upstream_branch)?;
     debug!("Pushed changes to fork");
 
     let base_branch = github_client.default_branch(options.organisation, options.repository)?;
-    let pull = github_client
-        .open_pr(options.organisation, options.repository, options.pr_title, base_branch.as_str(), &username, options.branch_name)?;
+    let pull = github_client.open_pr(
+        options.organisation,
+        options.repository,
+        options.pr_title,
+        base_branch.as_str(),
+        &username,
+        options.branch_name,
+    )?;
     debug!("Opened PR");
 
     let url = Url::parse(pull.url.as_str())?;
